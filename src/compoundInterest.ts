9e81aa0938a7ba01d090562196fd9e7f7a76f772
Pierre-Yves Verret <pyverret@gmail.com>
2022-01-09 21:36:25 -0500

    exports

--- src/compoundInterest.ts
+++ src/compoundInterest.ts
@@ -13,14 +13,14 @@
 // const r = 0.10 // Interest Rate
 // const n = 12; // Number of times the interest is applied per time period
 // const t = 2; // number of time periods elapsed in year
-type CompoundInterest = {
-    principal: number,
-    interestRate: number,
-    frequency: number,
-    years: number
-}
+type SimpleCompoundInterest = {
+    principal: number;
+    interestRate: number;
+    frequency: number;
+    years: number;
+};
 
-export function simpleCompoundInterest({ principal, interestRate, frequency, years }: CompoundInterest): number {
+export function simpleCompoundInterest({ principal, interestRate, frequency, years }: SimpleCompoundInterest): number {
     const compounded = principal * Math.pow(1 + interestRate / frequency, frequency * years);
 
     return Math.round(compounded * 1000) / 1000;
@@ -50,25 +50,53 @@ export function simpleCompoundInterest({ principal, interestRate, frequency, yea
 // console.log(details);
 
 // Detailed Basic Compound Interest Calculation With Additional contribution
-// value = P;
-// const detailsWithAdditionalContribution = [];
-// const additionalContribution = 10;
-// for (let i = 1; i <= months; i++) {
-//     const previousValue = value;
+type CompoundInterest = {
+    principal: number;
+    interestRate: number;
+    frequency: number;
+    years: number;
+    contribution?: number;
+};
 
-//     let number = value * Math.pow(1 + r / n, 1);
-//     value = (Math.round((number + additionalContribution) * 1000) / 1000);
+type CompoundInterestDetails = {
+    period: number;
+    month: number;
+    year: number;
+    initialValue: number;
+    additionalContributionAmount: number;
+    additionalContributionTotal: number;
+    value: number;
+    previousValue: number;
+};
 
-//     detailsWithAdditionalContribution.push({
-//         period: i,
-//         month: (i - 1) - parseInt((i - 1) / 12) * 12,
-//         year: parseInt((i - 1) / 12),
-//         initialValue: P,
-//         additionalContributionAmount: additionalContribution,
-//         additionalContributionTotal: additionalContribution * (i - 1),
-//         value: value,
-//         previousValue: previousValue,
-//     });
-// }
+export function detailedCompoundInterest({
+    principal,
+    contribution = 0,
+    interestRate,
+    frequency,
+    years,
+}: CompoundInterest): CompoundInterestDetails[] {
+    const detailsWithAdditionalContribution = [];
+    const months = years * frequency;
+    let value = principal;
 
-// console.log(detailsWithAdditionalContribution.length);
+    for (let i = 1; i <= months; i++) {
+        const previousValue = value;
+        const number = value * Math.pow(1 + interestRate / frequency, 1);
+
+        value = (Math.round((number + contribution) * 1000) / 1000);
+
+        detailsWithAdditionalContribution.push({
+            period: i,
+            month: (i - 1) - ((i - 1) / 12) * 12,
+            year: (i - 1) / 12,
+            initialValue: principal,
+            additionalContributionAmount: contribution,
+            additionalContributionTotal: contribution * (i - 1),
+            value: value,
+            previousValue: previousValue,
+        });
+    }
+
+    return detailsWithAdditionalContribution;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-import { simpleCompoundInterest } from './compoundInterest';
+export { simpleCompoundInterest, detailedCompoundInterest } from './compoundInterest';
 
-const P = 100; // Principal
-const r = 0.10 // Interest Rate
-const n = 12; // Number of times the interest is applied per time period
-const t = 2; // Time in year
+// const P = 100; // Principal
+// const r = 0.1; // Interest Rate
+// const n = 12; // Number of times the interest is applied per time period
+// const t = 2; // Time in year
+// const c = 10; // contribution
 
-console.log(simpleCompoundInterest({
-    principal: P,
-    interestRate: r,
-    frequency: n,
-    years: t
-}));
+// console.log(simpleCompoundInterest({
+//     principal: P,
+//     interestRate: r,
+//     frequency: n,
+//     years: t
+// }));
